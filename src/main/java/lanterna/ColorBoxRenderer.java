0077b8c17f7e204def4a3b3ef9ea9138796a08c1
ct-uni-st <cn.traub@gmail.com>
2025-01-23 19:06:39 +0100

    aaaaa

--- src/main/java/lanterna/ColorBoxRenderer.java
+++ src/main/java/lanterna/ColorBoxRenderer.java
@@ -31,26 +31,9 @@ public class ColorBoxRenderer extends TextBox.DefaultTextBoxRenderer {
         int xScrollOffset = getViewTopLeft().getColumn();
         int yScrollOffset = getViewTopLeft().getRow();
 
-        int lineNumber = 1 + yScrollOffset;
-        // to factor in previously read "-" signs into the calculation as yScrollOffset does not contain that information
-        int lineNumber2 = 1;
-        for(int i = 0; i < yScrollOffset; i++) {
-            if (i >= lines.length) break;
-            String line = lines[i];
-            String otherLine = otherLines[i];
-            if(line.isEmpty()) continue;
-            int symbolLocation = String.valueOf(lineNumber).length() + 2;
-            char symbol = line.charAt(symbolLocation);
-            char otherSymbol = otherLine.charAt(symbolLocation);
-            if(symbol != '-' && otherSymbol != '-') {
-                lineNumber2++;
-            } else {
-                lineNumber--;
-            }
-        }
+        int lineNumber = 1;
 
-
-        for (int i = yScrollOffset; i < yScrollOffset + textBox.getSize().getRows() - 1; i++) {
+        for (int i = 0; i < yScrollOffset + textBox.getSize().getRows() - 1; i++) {
             if (i >= lines.length) break;
             String line = lines[i];
             String otherLine = otherLines[i];
